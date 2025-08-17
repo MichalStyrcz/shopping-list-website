2d7f5994556de95ac0b10f688384a7fee6cb6358
MichalStyrcz <michal.styrcz@iongroup.com>
2025-08-17 12:09:51 +0200

    Corrected Product page layout

--- src/Product.js
+++ src/Product.js
@@ -7,29 +7,35 @@ function Product() {
       useContext(DataContext);
     return (
         <div className='Product'>
-            <label htmlFor='shop'>Sklep:</label>
-            <input
-              id='shop'
-                onChange={ (e) => setShop(e.target.value) }
-                placeholder='Sklep'
-                required
-                type='text'
-                value={ shop }/>
-            <label htmlFor='name'>Produkt:</label>
-            <input
-              id='name'
-                onChange={ (e) => setName(e.target.value) }
-                placeholder='Produkt'
-                required
-                type='text'
-                value={ name }/>
-            <label htmlFor='description'>Uwagi:</label>
-            <input
-              id='description'
-                onChange={ (e) => setDescription(e.target.value) }
-                placeholder='Uwagi'
-                type='text'
-                value={ description }/>
+            <div>
+                <label htmlFor='shop'>Sklep:</label>
+                <input
+                  id='shop'
+                    onChange={ (e) => setShop(e.target.value) }
+                    placeholder='Sklep'
+                    required
+                    type='text'
+                    value={ shop }/>
+            </div>
+            <div>
+                <label htmlFor='name'>Produkt:</label>
+                <input
+                  id='name'
+                    onChange={ (e) => setName(e.target.value) }
+                    placeholder='Produkt'
+                    required
+                    type='text'
+                    value={ name }/>
+            </div>
+            <div>
+                <label htmlFor='description'>Uwagi:</label>
+                <input
+                  id='description'
+                    onChange={ (e) => setDescription(e.target.value) }
+                    placeholder='Uwagi'
+                    type='text'
+                    value={ description }/>
+            </div>
         </div>
   );
 }
